b309120ec08f6e3c37cd1e5ed6ee47faad2b2d34
Tong Vuu <tong.vuu@terralogic.com>
2017-09-07 10:56:18 +0700

    Add fbchat from repo carpedm20/fbchat with modification about client id and method to wait authentication

---
+++ fbchat/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+from __future__ import unicode_literals
+from datetime import datetime
+from .client import *
+
+
+"""
+    fbchat
+    ~~~~~~
+
+    Facebook Chat (Messenger) for Python
+
+    :copyright: (c) 2015 by Taehoon Kim.
+    :license: BSD, see LICENSE for more details.
+"""
+
+
+__copyright__ = 'Copyright 2015 - {} by Taehoon Kim'.format(datetime.now().year)
+__version__ = '1.0.19'
+__license__ = 'BSD'
+__author__ = 'Taehoon Kim; Moreels Pieter-Jan; Mads Marquart'
+__email__ = 'carpedm20@gmail.com'
+__source__ = 'https://github.com/carpedm20/fbchat/'
+__description__ = 'Facebook Chat (Messenger) for Python'
+
+__all__ = [
+    'Client',
+]
